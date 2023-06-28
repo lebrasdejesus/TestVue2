304e304a2b9a4dabf97ace7783da3c263252db48
syneido <syneido@MacBook-Pro-de-syneido.local>
2023-06-28 17:53:40 +0200

    2eme test

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,47 @@ import TheWelcome from './components/TheWelcome.vue'
     </div>
   </header>
 
+<div>
+  <TheWelcome />
+</div>   
+</template>
+
+<style scoped> 
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}</style>
+
+
+<!-- 
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="frgthyjukilo" />
+    </div>
+  </header>
+
   <main>
     <TheWelcome />
   </main>
@@ -44,4 +85,4 @@ header {
     flex-wrap: wrap;
   }
 }
-</style>
+</style> -->
